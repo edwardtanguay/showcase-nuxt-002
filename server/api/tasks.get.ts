ae2c70c1678525b6879ac16940f7dd74127ed637
Edward Tanguay (windows) <edward@tanguay.info>
2026-02-05 00:27:22 +0100

    ..tasks in app

---
+++ server/api/tasks.get.ts
@@ -0,0 +1,11 @@
+export default defineEventHandler(async () => {
+	return [{
+		id: 1,
+		title: "Learn Nuxt",
+		completed: false,
+	}, {
+		id: 2,
+		title: "Learn Vue",
+		completed: false,
+	}];
+});
