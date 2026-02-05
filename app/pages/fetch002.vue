df45c4cca178d3cee1fece07b498d43c32894425
Edward Tanguay (windows) <edward@tanguay.info>
2026-02-05 23:16:06 +0100

    ..only sends error on error

--- app/pages/fetch002.vue
+++ app/pages/fetch002.vue
@@ -1,18 +1,26 @@
 <script lang="ts" setup>
-const { data: tasks, status } = useFetch("/api/fetch002", { lazy: true, server: false });
+const { data: tasks, status, refresh } = useFetch("/api/fetch002", { lazy: true, server: false, immediate: false });
+
+onMounted(() => {
+	refresh();
+});
+
 </script>
 
 <template>
-	<article>
+	<div>
 		<p>Status: {{ status }}</p>
-		<h2>Tasks from API:</h2>
-		<ul>
-			<li
-				v-for="task in tasks"
-				:key="task.id"
-			>
-				{{ task.title }}
-			</li>
-		</ul>
-	</article>
+		<article v-if="status === 'error'">
+			<p>Sorry, there was an error, please load the page again.</p>
+		</article>
+		<article v-if="status === 'success'">
+			<h2>Tasks from API:</h2>
+			<ul>
+				<li v-for="task in tasks"
+					:key="task.id">
+					{{ task.title }}
+				</li>
+			</ul>
+		</article>
+	</div>
 </template>
