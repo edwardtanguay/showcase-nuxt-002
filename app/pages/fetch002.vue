5db37de592127182923b024a9f99ea3d33587d74
Edward Tanguay (windows) <edward@tanguay.info>
2026-02-05 23:26:03 +0100

    ..002 title

--- app/pages/fetch002.vue
+++ app/pages/fetch002.vue
@@ -9,6 +9,10 @@ onMounted(() => {
 
 <template>
 	<div>
+		<h2>Fetch002</h2>
+		<p>
+			This is a fetch that is not loaded on the server. It is loaded on the client.
+		</p>
 		<p>Status: {{ status }}</p>
 		<article v-if="status === 'error'">
 			<p>Sorry, there was an error, please load the page again.</p>
