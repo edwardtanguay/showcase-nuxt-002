71db6bfe232a963dd8ce5ae8bab62a6761c4bca2
Edward Tanguay (windows) <edward@tanguay.info>
2026-02-05 23:24:51 +0100

    ..001 ok

--- app/pages/fetch001.vue
+++ app/pages/fetch001.vue
@@ -3,17 +3,20 @@ const { data: tasks } = useFetch("/api/fetch001");
 </script>
 
 <template>
-	<article>
+	<div>
 		<h2>Fetch001</h2>
 		<p>
-			This is he simplest fest, simply getting a static array of objects from the API route. Note that it loads
+			This is he simplest fetch, simply getting a static array of objects from the API route. Note that it
+			loads
 			fast and has no hydration errors.
 		</p>
-		<ul>
-			<li v-for="task in tasks"
-				:key="task.id">
-				{{ task.title }}
-			</li>
-		</ul>
-	</article>
+		<article>
+			<ul>
+				<li v-for="task in tasks"
+					:key="task.id">
+					{{ task.title }}
+				</li>
+			</ul>
+		</article>
+	</div>
 </template>
